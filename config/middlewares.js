62b01976ca59f3591108e48dea18a5769b7afde0
Praveen Kumar Jasthi <praveen.j@VDARTINC.COM>
2025-07-15 15:56:17 +0530

    Configure CORS via environment variable

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,30 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'], // Add any external image hosts if you use them
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'], // Add any external media hosts
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : ['*'], // Read origins from env variable
+      headers: '*', // Allow all headers
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'], // Allowed methods
+      credentials: true, // Allow cookies/auth headers
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
